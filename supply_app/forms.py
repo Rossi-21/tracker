45c7c1ad7e6218862ec38ae1c5499359ba98636e
Rossi21 <rossi21@gmail.com>
2024-01-11 16:36:51 -0800

    form filtering

--- supply_app/forms.py
+++ supply_app/forms.py
@@ -8,3 +8,11 @@ class InvoiceCreateForm(ModelForm):
 
         model = Invoice
         fields = ['vendor', 'department', 'total', 'date']
+
+
+class InvoiceFilterForm(forms.Form):
+    total_gt = forms.FloatField(required=False, label='Total greater than')
+    date_start = forms.DateField(required=False, label='Start date')
+    date_end = forms.DateField(required=False, label='End date')
+    vendor_name = forms.CharField(
+        max_length=128, required=False, label='Vendor name')
--- supply_app/views.py
+++ supply_app/views.py
@@ -5,8 +5,28 @@ from .forms import *
 
 def home(request):
     invoices = Invoice.objects.all()
+    form = InvoiceFilterForm()
+    if request.method == 'POST':
+        form = InvoiceFilterForm(request.POST)
+        if form.is_valid():
+            # Build the filter parameters based on form input
+            filter_params = {}
+            if form.cleaned_data['total_gt']:
+                filter_params['total__gt'] = form.cleaned_data['total_gt']
+            if form.cleaned_data['date_start']:
+                filter_params['date__gte'] = form.cleaned_data['date_start']
+            if form.cleaned_data['date_end']:
+                filter_params['date__lte'] = form.cleaned_data['date_end']
+            if form.cleaned_data['vendor_name']:
+                filter_params['vendor__name__icontains'] = form.cleaned_data['vendor_name']
+
+            # Query the database with the filter parameters
+            filtered_invoices = Invoice.objects.filter(**filter_params)
+
+            return render(request, 'index.html', {'invoices': filtered_invoices, 'form': form})
 
     context = {
-        'invoices': invoices
+        'invoices': invoices,
+        'form': form,
     }
     return render(request, 'index.html', context)
